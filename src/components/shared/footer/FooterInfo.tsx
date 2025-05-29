a338fad46b2e996f6b33ac1a41fa2dd4aacd3175
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-05-29 16:39:48 +0300

    updated footer contacts

--- src/components/shared/footer/FooterInfo.tsx
+++ src/components/shared/footer/FooterInfo.tsx
@@ -1,8 +1,10 @@
 import {
   PHONE,
+  PHONE_SECOND,
+  PHONE_THIRD,
+  PHONE_FOURTH,
   EMAIL,
   ADDRESS_LINK_FIRST,
-  ADDRESS_LINK_SECOND,
   ADDRESS_SECOND,
   ADDRESS_FIRST,
 } from "@/constants/constants";
@@ -12,19 +14,50 @@ export default function FooterInfo() {
   return (
     <ul className="flex flex-col md:flex-row md:justify-between xl:gap-x-10 text-14light md:text-16light leading-[120%] text-white">
       <li>
-        <div className="mb-4">
-          Тел.:&nbsp;
-          <a
-            href={`tel:+${PHONE.replace(/\D/g, "")}`}
-            target="_blank"
-            rel="noopener noreferrer nofollow"
-            aria-label="phone number"
-            className="lg:hover:text-green focus-visible:text-green transition duration-300 ease-in-out"
-          >
-            {PHONE.replace(headerPhoneRegex, "$1-$2-$3-$4-$5")}
-          </a>
+        <div className="flex mb-4">
+          <p>Тел.:&nbsp;</p>
+          <div>
+            <a
+              href={`tel:+${PHONE.replace(/\D/g, "")}`}
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+              aria-label="phone number"
+              className="block mb-1 lg:hover:text-green focus-visible:text-green transition duration-300 ease-in-out"
+            >
+              {PHONE.replace(headerPhoneRegex, "$1-$2-$3-$4-$5")}
+            </a>
+            <a
+              href={`tel:+${PHONE.replace(/\D/g, "")}`}
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+              aria-label="phone number"
+              className="block mb-1 lg:hover:text-green focus-visible:text-green transition duration-300 ease-in-out"
+            >
+              {PHONE_SECOND.replace(headerPhoneRegex, "$1-$2-$3-$4-$5")}
+            </a>
+            <a
+              href={`tel:+${PHONE.replace(/\D/g, "")}`}
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+              aria-label="phone number"
+              className="block mb-1 lg:hover:text-green focus-visible:text-green transition duration-300 ease-in-out"
+            >
+              {PHONE_THIRD.replace(headerPhoneRegex, "$1-$2-$3-$4-$5")}
+            </a>
+            <a
+              href={`tel:+${PHONE.replace(/\D/g, "")}`}
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+              aria-label="phone number"
+              className="block mb-1 lg:hover:text-green focus-visible:text-green transition duration-300 ease-in-out"
+            >
+              {PHONE_FOURTH.replace(headerPhoneRegex, "$1-$2-$3-$4-$5")}
+            </a>
+          </div>
         </div>
-        <div>
+      </li>
+      <li>
+        <div className="mb-4">
           Email:&nbsp;
           <a
             href={`mailto:${EMAIL}`}
@@ -36,24 +69,14 @@ export default function FooterInfo() {
             {EMAIL}
           </a>
         </div>
-      </li>
-      <li className="mt-auto">
         <a
           href={ADDRESS_LINK_FIRST}
           target="_blank"
           rel="noopener noreferrer nofollow"
-          className="block mb-4 lg:hover:text-green focus-visible:text-green transition duration-300 ease-in-out"
+          className="block lg:hover:text-green focus-visible:text-green transition duration-300 ease-in-out"
           aria-label="address"
         >
-          <p className="mb-2">{ADDRESS_FIRST}</p>
-        </a>
-        <a
-          href={ADDRESS_LINK_SECOND}
-          target="_blank"
-          rel="noopener noreferrer nofollow"
-          className="lg:hover:text-green focus-visible:text-green transition duration-300 ease-in-out"
-          aria-label="address"
-        >
+          <p>{ADDRESS_FIRST}</p>
           <p>{ADDRESS_SECOND}</p>
         </a>
       </li>
--- src/components/shared/footer/SocialsList.tsx
+++ src/components/shared/footer/SocialsList.tsx
@@ -1,14 +1,21 @@
 import InstagramIcon from "../icons/InstagramIcon";
 import TelegramIcon from "../icons/TelegramIcon";
 import TiktokIcon from "../icons/TiktokIcon";
-import { TIKTOK_URL, INSTAGRAM_URL, TELEGRAM_URL } from "@/constants/constants";
+import {
+  TIKTOK_URL,
+  INSTAGRAM_URL,
+  TELEGRAM_URL,
+  VIBER_URL,
+} from "@/constants/constants";
 import SocialItem from "./SocialItem";
+import IconViber from "../icons/IconViber";
 
 export default function SocialsList() {
   const socialList = [
     { link: TIKTOK_URL, icon: <TiktokIcon /> },
     { link: INSTAGRAM_URL, icon: <InstagramIcon /> },
     { link: TELEGRAM_URL, icon: <TelegramIcon /> },
+    { link: VIBER_URL, icon: <IconViber /> },
   ];
   return (
     <ul className="flex gap-7 md:gap-[18px] items-center justify-center md:justify-end mt-8 md:mt-7">
